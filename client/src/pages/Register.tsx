6bb7fb4730ba45e774fbc56838452929455a22b8
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-04 10:34:08 +0100

    pass header text as header prop

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,9 +1,15 @@
 import Navbar from "../components/Navbar"
+import Header from "../components/Header"
 
 function Register() {
     return (
-        <div>
+        <div className="min-h-screen">
             <Navbar />
+            <div className="grid grid-cols-12 gap-6 p-6">
+                <div className='col-span-12'>
+                    <Header children={'Register a new account'}/>
+                </div>
+            </div>
         </div>
     )
 }
