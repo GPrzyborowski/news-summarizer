ee98b7a3a6f202b279cb68e6757dc2f3fddd23a9
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-05 20:48:55 +0100

    add login form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,15 @@
 import UpperContainer from '../components/UpperContainer'
+import LoginForm from '../components/LoginForm'
 
 function Login() {
 	return (
 		<>
 			<UpperContainer title={'Login to your account'} />
+			<div className="flex justify-center mt-70 px-6">
+				<div className="w-full max-w-md flex justify-center">
+					<LoginForm />
+				</div>
+			</div>
 		</>
 	)
 }
