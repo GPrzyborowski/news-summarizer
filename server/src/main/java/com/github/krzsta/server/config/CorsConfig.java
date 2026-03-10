7aaa30d934c6186095af688b6b57bd17bc28d214
Krzysztof Stasiak <krzsta@proton.me>
2026-03-10 14:54:43 +0100

    Refactor CORS configuration method and update allowed headers

--- server/src/main/java/com/github/krzsta/server/config/CorsConfig.java
+++ server/src/main/java/com/github/krzsta/server/config/CorsConfig.java
@@ -5,14 +5,13 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
     
     @Bean
-    public CorsConfigurationSource configurationSource() {
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
@@ -25,9 +24,7 @@ public class CorsConfig {
             "GET", "POST", "PUT", "DELETE", "OPTIONS"
         ));
 
-        configuration.setAllowedHeaders(List.of(
-            "Authorization", "Content-Type", "Accept", "Origin"
-        ));
+        configuration.setAllowedHeaders(List.of("*"));
 
         configuration.setExposedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
