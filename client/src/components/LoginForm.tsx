5b8804b527ec6573d48992454d750c3af4cb538d
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-03-15 02:46:36 +0100

    add login function

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,19 +1,43 @@
 import InputBox from './InputBox'
+import { API_URL } from '../config/env'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function LoginForm() {
-	const [login, setLogin] = useState('')
+	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 
+	const navigate = useNavigate()
+
+	const LOGIN_ENDPOINT = `${API_URL}/auth/login`
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		const res = await fetch(LOGIN_ENDPOINT, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				email: email,
+				password: password,
+			}),
+		})
+		if (!res.ok) {
+			const data = await res.json()
+			console.log(data.message)
+			return
+		}
+		navigate('/')
+	}
+
 	return (
-		<form action="">
+		<form onSubmit={handleSubmit}>
 			<InputBox
-				htmlFor="login"
-				labelText="Login"
+				htmlFor="email"
+				labelText="Email"
 				type="text"
-				id="login"
-				value={login}
-				onChange={e => setLogin(e.target.value)}
+				id="email"
+				value={email}
+				onChange={e => setEmail(e.target.value)}
 			/>
 			<InputBox
 				htmlFor="password"
